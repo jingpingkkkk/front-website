e16e9d700678423270c8cec89b9a9297dbcf3f8a
pikachu <sumeet@rethinksoft.com>
2023-09-06 16:35:29 +0530

    add encryption to request response

--- src/api/encryption.js
+++ src/api/encryption.js
@@ -1,38 +1,54 @@
-/* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
+/* eslint-disable no-plusplus */
 import CryptoJS from 'crypto-js';
+import { nanoid } from 'nanoid';
 
-const dataProp = 'cpr_ctx';
+const frag = (str, fragmentSize = 8) => {
+  const arr = [];
+  let keyIndex = 0;
+  for (let i = 0; i < str.length; i += fragmentSize) {
+    const fragment = str.slice(i, i + fragmentSize);
+    const key = nanoid(fragmentSize);
+    if (i % 3 === 0) {
+      arr.push({ [fragment]: { [keyIndex++]: key } });
+    } else {
+      arr.push({ [key]: { [keyIndex++]: fragment } });
+    }
+  }
+  return arr;
+};
 
-const mergeArrFragmentsToStr = (arr) => {
+const defrag = (arr) => {
   let str = '';
-  for (const obj of arr) {
-    for (const key in obj) {
-      const sortedKeys = Object.keys(obj[key]).sort((a, b) => a - b);
-      for (const keyIndex of sortedKeys) {
-        str += obj[key][keyIndex];
+  for (let i = 0; i < arr.length; i++) {
+    const obj = arr[i];
+    for (const [key, value] of Object.entries(obj)) {
+      if (i % 3 === 0) {
+        str += key;
+      } else {
+        const sortedKeys = Object.keys(value).sort((a, b) => a - b);
+        for (const keyIndex of sortedKeys) {
+          str += value[keyIndex];
+        }
       }
     }
   }
   return str;
 };
 
+const resAes = import.meta.env.VITE_RESPONSE_AES_SECRET;
+
 const encryptRequest = (data = {}) => {
-  const encrypted = CryptoJS.AES.encrypt(
-    JSON.stringify({ message: data }),
-    import.meta.env.VITE_RESPONSE_AES_SECRET,
-  );
-  return encrypted.toString();
+  const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), resAes);
+  const cipherText = encrypted.toString();
+  return JSON.stringify({ payload: frag(cipherText) });
 };
 
 const decryptResponse = (data = {}) => {
-  const mergedString = mergeArrFragmentsToStr(data[dataProp]);
-  const decrypted = CryptoJS.AES.decrypt(
-    mergedString,
-    import.meta.env.VITE_RESPONSE_AES_SECRET,
-  );
+  const mergedString = defrag(data?.cpr_ctx);
+  const decrypted = CryptoJS.AES.decrypt(mergedString, resAes);
   const parsed = JSON.parse(decrypted.toString(CryptoJS.enc.Utf8));
   return parsed;
 };
 
-export { dataProp, decryptResponse, encryptRequest, mergeArrFragmentsToStr };
+export { decryptResponse, encryptRequest };
