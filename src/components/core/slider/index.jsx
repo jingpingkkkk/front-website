5006ad4762bf806154721c390934099374387aa9
Shilpa-Hoch <137758395+Shilpa-Hoch@users.noreply.github.com>
2023-09-21 10:45:38 +0530

    hide slider if slider image array is empty

--- src/components/core/slider/index.jsx
+++ src/components/core/slider/index.jsx
@@ -5,26 +5,30 @@ import { Carousel } from 'react-responsive-carousel';
 
 const BannerSlider = () => {
   const { themeSettings } = useSelector((state) => state.themeSettings);
-  const sliderImages = themeSettings?.bannerImages || ['images/banner1.png'];
+  const sliderImages = themeSettings?.bannerImages || [];
   return (
     <div style={{ borderRadius: '8px', overflow: 'hidden' }}>
-      <Carousel
-        showArrows={false}
-        showStatus={false}
-        infiniteLoop
-        showThumbs={false}
-        autoPlay
-        stopOnHover
-        swipeable
-        dynamicHeight
-        emulateTouch
-      >
-        {sliderImages?.map((banner, index) => (
-          <div className="px-1" key={index}>
-            <img src={banner} alt="banner" />
-          </div>
-        ))}
-      </Carousel>
+      {sliderImages?.length ? (
+        <Carousel
+          showArrows={false}
+          showStatus={false}
+          infiniteLoop
+          showThumbs={false}
+          autoPlay
+          stopOnHover
+          swipeable
+          dynamicHeight
+          emulateTouch
+        >
+          {sliderImages?.map((banner, index) => (
+            <div className="px-1" key={index}>
+              <img src={banner} alt="banner" />
+            </div>
+          ))}
+        </Carousel>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
