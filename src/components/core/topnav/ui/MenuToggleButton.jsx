a58e1e29fc1a218b29577c97dbf42281544b39e2
Shilpa-Hoch <137758395+Shilpa-Hoch@users.noreply.github.com>
2023-10-25 12:51:48 +0530

    and home button

--- src/components/core/topnav/ui/MenuToggleButton.jsx
+++ src/components/core/topnav/ui/MenuToggleButton.jsx
@@ -3,20 +3,25 @@ import ExchangeSideMenu from '../../../common/exchange-sidemenu';
 
 function MenuToggleButton() {
   const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <>
       <div className="mobile-toggler" style={{ display: 'none' }}>
-        <button
-          type="button"
-          className="navbar-toggle collapsed"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="visually-hidden-focusable">Toggle navigation</span>
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
+        {window?.location?.pathname !== '/sports' ? (
+          <a href="/" className="navbar-toggle ps-0">
+            <img src="/images/icon-home.png" alt="home" className="home-icon" />
+          </a>
+        ) : (
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span className="visually-hidden-focusable">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+        )}
       </div>
 
       <div className={menuOpen ? 'mobile-sidebar-container' : 'd-none'}>
