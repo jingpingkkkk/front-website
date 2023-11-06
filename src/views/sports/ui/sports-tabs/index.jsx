ae5f63c80d342f683b9dbc27c45f344af1a96130
Shilpa-Hoch <137758395+Shilpa-Hoch@users.noreply.github.com>
2023-11-06 14:32:20 +0530

    fix slider issue

--- src/views/sports/ui/sports-tabs/index.jsx
+++ src/views/sports/ui/sports-tabs/index.jsx
@@ -8,28 +8,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 function SportsTabs({ availableSports, onClick }) {
-  const {
-    isMobile,
-    isTablet,
-    isDesktop,
-    isLargeDesktop,
-    isExtraLargeDesktop,
-    isExtra2LargeDesktop,
-    width,
-  } = useScreenWidth();
-  const perView = isMobile
-    ? 3
-    : isTablet
-    ? 7
-    : isDesktop
-    ? 6
-    : isLargeDesktop
-    ? 6
-    : isExtraLargeDesktop
-    ? 9
-    : isExtra2LargeDesktop
-    ? 15
-    : 11;
+  const { width } = useScreenWidth();
 
   const [sportId, setSportId] = useState(availableSports[0]?._id);
 
@@ -38,79 +17,91 @@ function SportsTabs({ availableSports, onClick }) {
     onClick(id, name);
   };
 
+  let slidesToShow = 10; // Default for larger screens
+  if (width <= 320) {
+    slidesToShow = 3;
+  } else if (width <= 425 && width > 320) {
+    slidesToShow = 4;
+  } else if (width < 1024) {
+    slidesToShow = 8;
+  } else if (width < 1280) {
+    slidesToShow = 6;
+  } else if (width < 1920) {
+    slidesToShow = 9;
+  } else if (width >= 1920 && width < 2560) {
+    slidesToShow = 12;
+  } else if (width <= 2560) {
+    slidesToShow = 16;
+  }
+
   return (
-    <ul>
-      <Slider
-        speed={500}
-        dots={false}
-        infinite={false}
-        draggable={false}
-        swipe
-        slidesToShow={perView}
-        slidesToScroll={perView}
-        autoplay={false}
-        cssEase="linear"
-        responsive={[
-          {
-            breakpoint: width,
-            slidesToShow: perView,
-            slidesToScroll: perView,
-            infinite: false,
+    <Slider
+      speed={500}
+      dots={false}
+      infinite={false}
+      draggable
+      swipe
+      slidesToShow={slidesToShow}
+      slidesToScroll={2}
+      autoplay={false}
+      cssEase="linear"
+      initialSlide={0}
+      responsive={[
+        {
+          breakpoint: width,
+          settings: {
+            slidesToShow,
           },
-        ]}
-        className="sport-tabs multiSportSlider"
-      >
-        <li className="nav-item item">
-          <button
-            type="button"
-            style={{ all: 'unset', cursor: 'pointer' }}
-            onClick={() => onClickSport(1, 'In Play')}
-          >
-            <div className="tab-main active">
-              <img src={menuImages['In Play']} alt="In Play" />
+        },
+      ]}
+      className="sport-tabs multiSportSlider"
+    >
+      <div className="nav-item item">
+        <button
+          type="button"
+          style={{ all: 'unset', cursor: 'pointer' }}
+          onClick={() => onClickSport(1, 'In Play')}
+        >
+          <div className="tab-main active">
+            <img src={menuImages['In Play']} alt="In Play" />
 
-              <div className="title-area">In Play</div>
+            <div className="title-area">In Play</div>
 
-              <div className="remark">
-                <span className="totel me-0">0</span>
-                <span className="out-of">0</span>
-              </div>
+            <div className="remark">
+              <span className="totel me-0">0</span>
+              <span className="out-of">0</span>
             </div>
-          </button>
-        </li>
-        {availableSports?.map((sport, index) => {
-          const imgPath = menuImages[sport?.name] || '';
-          return (
-            <li className="nav-item item" key={`${sport.name} ${index}`}>
-              <button
-                type="button"
-                style={{ all: 'unset', cursor: 'pointer' }}
-                onClick={() => onClickSport(sport?._id, sport.name)}
+          </div>
+        </button>
+      </div>
+      {availableSports?.map((sport, index) => {
+        const imgPath = menuImages[sport?.name] || '';
+        return (
+          <div className="nav-item item" key={`${sport.name} ${index}`}>
+            <button
+              type="button"
+              style={{ all: 'unset', cursor: 'pointer' }}
+              onClick={() => onClickSport(sport?._id, sport.name)}
+            >
+              <div
+                className={`tab-main ${sport?._id === sportId ? 'active' : ''}`}
               >
-                <div
-                  className={`tab-main ${
-                    sport?._id === sportId ? 'active' : ''
-                  }`}
-                >
-                  <img src={imgPath} alt={sport.name} />
+                <img src={imgPath} alt={sport.name} />
 
-                  <div className="title-area">{sport?.name || ''}</div>
+                <div className="title-area">{sport?.name || ''}</div>
 
-                  <div className="remark">
-                    <span className="totel me-0">
-                      {sport?.getAllActiveEvent || 0}
-                    </span>
-                    <span className="out-of">
-                      {sport?.getAllLiveEvent || 0}
-                    </span>
-                  </div>
+                <div className="remark">
+                  <span className="totel me-0">
+                    {sport?.getAllActiveEvent || 0}
+                  </span>
+                  <span className="out-of">{sport?.getAllLiveEvent || 0}</span>
                 </div>
-              </button>
-            </li>
-          );
-        })}
-      </Slider>
-    </ul>
+              </div>
+            </button>
+          </div>
+        );
+      })}
+    </Slider>
   );
 }
 
